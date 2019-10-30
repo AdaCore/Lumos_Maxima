facc2496d0ed56421f6f94a5b82924cd5109d6b4
Sylvain Dailler <sylvain.dailler@gmail.com>
2019-10-30 12:56:34 +0100

    Remove confirm_code_gen

--- why3/extract/confirm_code_gen.ml
+++
@@ -1,6 +0,0 @@
-let gen =
-  let e = ref 0 in
-  let gen () =
-    e := !e + 1;
-    !e in
-  (fun () -> gen ())
--- why3/extract/prelude.ml
+++ why3/extract/prelude.ml
@@ -10,3 +10,11 @@ module Set = Set.Make (OrdP)
 
 let defensive_find h k =
   Hashtbl.find h k
+
+(* Confirmation code generation: TODO *)
+let gen =
+  let e = ref 0 in
+  let gen () =
+    e := !e + 1;
+    !e in
+  (fun () -> gen ())
